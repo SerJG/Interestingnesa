710897535ae0a0752f1c18ddbebd1db82809bc83
Sergii Gordiienko <serjgord@gmail.com>
2022-01-29 21:41:51 +0200

    Add basic view controllers

--- Interestingness/LoadImagesViewController.swift
+++ Interestingness/LoadImagesViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoadImagesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Interestingness/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  Interestingness
+//
+//  Created by Sergii Gordiienko on 29.01.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
